58faab518bbcf9dfda2ca768e7d328ab6eca011a
jaemin4 <eheh25877@gmail.com>
2025-03-26 00:35:40 +0900

    feature fix : pointById를 실행했을때 값이 없으면 생성되는 구조이기 때문에 null이 반환될 수 없음 -> null체크 삭제

--- src/main/java/io/hhplus/tdd/service/front/UserPointFrontService.java
+++ src/main/java/io/hhplus/tdd/service/front/UserPointFrontService.java
@@ -25,15 +25,6 @@ public class UserPointFrontService {
 
     public RestResult getPointById(Long id) {
         UserPoint resultUserPoint = userPointService.getPointById(id);
-
-    /*
-    TODO - 1.checkPoint
-        id validation 포함해서 사용자가 없으면 생성하는 구조이기 때문에 의미 없을지도?
-    */
-        if (resultUserPoint == null) {
-            throw new UserPointRuntimeException("사용자를 찾을 수 없습니다.");
-        }
-
         return new RestResult("200", "Retrieve success", Map.of("data", resultUserPoint));
     }
 
@@ -74,7 +65,6 @@ public class UserPointFrontService {
         }
         Lock lock = userLocks.computeIfAbsent(id,k-> new ReentrantLock());
         lock.lock();
-
         try{
             UserPoint resultUserPoint = userPointService.getPointById(id);
 
