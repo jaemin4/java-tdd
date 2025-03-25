23b218d094a70171bd0fc13c499914aa778330b7
jaemin4 <eheh25877@gmail.com>
2025-03-25 20:58:06 +0900

    add exception : UserPointRuntimeException 추가

---
+++ src/main/java/io/hhplus/tdd/exception/UserPointRuntimeException.java
@@ -0,0 +1,8 @@
+package io.hhplus.tdd.exception;
+
+public class UserPointRuntimeException extends CustomRuntimeException{
+
+    public UserPointRuntimeException(String message){
+        super(message);
+    }
+}
