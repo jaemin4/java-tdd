abdf8461723c17b55ce2b62dec463398c0692dd0
jaemin4 <eheh25877@gmail.com>
2025-03-25 21:05:48 +0900

    feature : 특정 유저의 포인트를 조회(history)

--- src/main/java/io/hhplus/tdd/service/front/UserPointFrontService.java
+++ src/main/java/io/hhplus/tdd/service/front/UserPointFrontService.java
@@ -3,17 +3,19 @@ package io.hhplus.tdd.service.front;
 import io.hhplus.tdd.exception.UserPointRuntimeException;
 import io.hhplus.tdd.model.entity.UserPoint;
 import io.hhplus.tdd.model.result.RestResult;
+import io.hhplus.tdd.service.persist.PointHistoryService;
 import io.hhplus.tdd.service.persist.UserPointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.Map;
-
 
 @Service
 @RequiredArgsConstructor
 public class UserPointFrontService {
 
     private final UserPointService userPointService;
+    private final PointHistoryService pointHistoryService;
+
 
     public RestResult getPointById(Long id) {
         UserPoint resultUserPoint = userPointService.getPointById(id);
@@ -27,6 +29,12 @@ public class UserPointFrontService {
         }
 
         return new RestResult("200", "Retrieve success", Map.of("data", resultUserPoint));
+    }
+
+    public RestResult RetrieveUserHistoryById(Long userId) {
+        return new RestResult("200",
+                "Retrieve UserHistory Success",
+                Map.of("data", pointHistoryService.selectAllByUserId(userId)));
     }
 
 
